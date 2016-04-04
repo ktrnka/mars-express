5bd29724ed8a74ad6fa30d5a1758d7cb5d6471b8
Keith Trnka <keith.trnka@gmail.com>
2016-04-04 15:27:16 -0700

    Messy version of doing test set prediction

--- src/train_test.py
+++ src/train_test.py
@@ -20,6 +20,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--extra-analysis", default=False, action="store_true", help="Extra analysis on the data")
     parser.add_argument("training_dir", help="Dir with the training CSV files")
+    parser.add_argument("testing_dir", help="Dir with the testing files, including the empty prediction file")
+    parser.add_argument("prediction_file", help="Destination for predictions")
     return parser.parse_args()
 
 
@@ -45,18 +47,17 @@ def load_series(files, add_file_number=False, resample_interval=None):
     return pandas.concat(data)
 
 
-def load_data(training_dir):
+def load_data(data_dir, resample_interval=None):
     # load the base power data
-    data = load_series(find_files(training_dir, "power"), add_file_number=True, resample_interval="1H")
+    data = load_series(find_files(data_dir, "power"), add_file_number=True, resample_interval=resample_interval)
 
-    saaf_data = load_series(find_files(training_dir, "saaf"))
-    longterm_data = load_series(find_files(training_dir, "ltdata"))
+    saaf_data = load_series(find_files(data_dir, "saaf"))
+    longterm_data = load_series(find_files(data_dir, "ltdata"))
 
     for other_data in [saaf_data, longterm_data]:
         data = pandas.concat([data, other_data.reindex(data.index, method="nearest")], axis=1)
 
     data["days_in_space"] = (data.index - pandas.datetime(year=2003, month=6, day=2)).days
-    # data["days_in_space_log"] = numpy.log(data["days_in_space"])
 
     return data.fillna(data.mean())
 
@@ -79,33 +80,31 @@ def compute_upper_bounds(data):
 def main():
     args = parse_args()
 
-    power_data = load_data(args.training_dir)
+    train_data = load_data(args.training_dir, resample_interval="1H")
 
     # cross validation by year
-    splits = sklearn.cross_validation.LeaveOneLabelOut(power_data["file_number"])
-    power_data.drop("file_number", axis=1, inplace=True)
+    splits = sklearn.cross_validation.LeaveOneLabelOut(train_data["file_number"])
 
     # just use the biggest one for now
-    Y = power_data[[col for col in power_data.columns if col.startswith("NPWD")]]
-    X = power_data[[col for col in power_data.columns if not col.startswith("NPWD")]]
+    X_train, Y_train = separate_output(train_data)
 
     if args.extra_analysis:
-        X.info()
-        print X.describe()
+        X_train.info()
+        print X_train.describe()
 
-    X = sklearn.preprocessing.StandardScaler().fit_transform(X)
+    X_train = sklearn.preprocessing.StandardScaler().fit_transform(X_train)
 
     # lower bound: predict mean
     baseline = sklearn.dummy.DummyRegressor("mean")
-    baseline_scores = mse_to_rms(sklearn.cross_validation.cross_val_score(baseline, X, Y, scoring="mean_squared_error", cv=splits))
+    baseline_scores = mse_to_rms(sklearn.cross_validation.cross_val_score(baseline, X_train, Y_train, scoring="mean_squared_error", cv=splits))
     print "DummyRegressor(mean): {:.3f} +/- {:.3f}".format(baseline_scores.mean(), baseline_scores.std())
 
     # upper bound: predict average per diem
     if args.extra_analysis:
-        compute_upper_bounds(power_data)
+        compute_upper_bounds(train_data)
 
     model = sklearn.linear_model.LinearRegression()
-    scores = mse_to_rms(sklearn.cross_validation.cross_val_score(model, X, Y, scoring="mean_squared_error", cv=splits))
+    scores = mse_to_rms(sklearn.cross_validation.cross_val_score(model, X_train, Y_train, scoring="mean_squared_error", cv=splits))
     print "LinearRegression: {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
 
     # model = sklearn.linear_model.PassiveAggressiveRegressor()
@@ -113,15 +112,40 @@ def main():
     # print "PA: {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
 
 
-    model = sklearn.ensemble.RandomForestRegressor(20, min_samples_leaf=100, max_depth=3)
-    scores = mse_to_rms(sklearn.cross_validation.cross_val_score(model, X, Y, scoring="mean_squared_error", cv=splits))
-    print "RandomForestRegression: {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
+    # model = sklearn.ensemble.RandomForestRegressor(20, min_samples_leaf=100, max_depth=3)
+    # scores = mse_to_rms(sklearn.cross_validation.cross_val_score(model, X, Y, scoring="mean_squared_error", cv=splits))
+    # print "RandomForestRegression: {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
+    #
+    # wrapped_model = sklearn.grid_search.RandomizedSearchCV(model, {"min_samples_leaf": range(10, 100), "max_depth": range(3, 10), "max_features": ["sqrt", "log2", 1.0, 0.3]}, n_iter=20)
+    # scores = mse_to_rms(sklearn.cross_validation.cross_val_score(wrapped_model, X, Y, scoring="mean_squared_error", cv=splits))
+    # print "RandomForestRegression(tuned): {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
 
-    wrapped_model = sklearn.grid_search.RandomizedSearchCV(model, {"min_samples_leaf": range(10, 100), "max_depth": range(3, 10), "max_features": ["sqrt", "log2", 1.0, 0.3]}, n_iter=20)
-    scores = mse_to_rms(sklearn.cross_validation.cross_val_score(wrapped_model, X, Y, scoring="mean_squared_error", cv=splits))
-    print "RandomForestRegression(tuned): {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
+    # load the test data and run predictions
+    test_data = load_data(args.testing_dir)
+    print test_data.describe()
+
+    X_test, Y_test = separate_output(test_data)
+
+    # retrain a model on the full data
+    baseline = sklearn.dummy.DummyRegressor("mean")
+    baseline.fit(X_train, Y_train)
+
+    test_data[Y_train.columns] = baseline.predict(X_test)
+
+    print "Test data describe", test_data.describe()
+
+    print "Train data describe", train_data.describe()
+
+    test_data.index = test_data.index.astype(numpy.int64) / 10 ** 6
+    test_data[Y_test.columns].to_csv(args.prediction_file, index_label="ut_ms")
 
 
+def separate_output(dataframe):
+    dataframe.drop("file_number", axis=1, inplace=True)
+
+    Y = dataframe[[col for col in dataframe.columns if col.startswith("NPWD")]]
+    X = dataframe[[col for col in dataframe.columns if not col.startswith("NPWD")]]
+    return X, Y
 
 
 if __name__ == "__main__":
