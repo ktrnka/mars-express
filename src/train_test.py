598865a23f882c7dc74f38684da7bfbffb2bd926
Keith Trnka <keith.trnka@gmail.com>
2016-04-05 13:02:56 -0700

    Pulling out some shared code

--- src/train_test.py
+++ src/train_test.py
@@ -3,6 +3,7 @@ import sys
 import argparse
 
 import collections
+from operator import itemgetter
 
 import pandas
 import sklearn
@@ -124,21 +125,38 @@ def compute_upper_bounds(data):
         rms = ((data - upsampled_data) ** 2).mean().mean() ** 0.5
         print "RMS with {} approximation: {:.3f}".format(interval, rms)
 
-class VectorRegression(sklearn.base.BaseEstimator):
+
+class MultivariateRegressionWrapper(sklearn.base.BaseEstimator):
+    """
+    Wrap a univariate regression model to support multivariate regression.
+    Tweaked from http://stats.stackexchange.com/a/153892
+    """
     def __init__(self, estimator):
         self.estimator = estimator
         self.estimators_ = None
 
-    def fit(self, X, y):
-        n, m = y.shape
-        # Fit a separate regressor for each column of y
-        self.estimators_ = [sklearn.base.clone(self.estimator).fit(X, y[:, i]) for i in range(m)]
+    def fit(self, X, Y):
+        if isinstance(Y, pandas.DataFrame):
+            Y = Y.values
+
+        assert len(Y.shape) == 2
+        self.estimators_ = [sklearn.base.clone(self.estimator).fit(X, Y[:, i]) for i in xrange(Y.shape[1])]
         return self
 
     def predict(self, X):
-        # Join regressors' predictions
-        res = [est.predict(X)[:, numpy.newaxis] for est in self.estimators_]
-        return numpy.hstack(res)
+        result = numpy.hstack([estimator.predict(X)[:, numpy.newaxis] for estimator in self.estimators_])
+
+        assert result.shape[0] == X.shape[0]
+        assert result.shape[1] == len(self.estimators_)
+
+        return result
+
+def print_tuning_scores(tuned_estimator, reverse=True):
+    """Show the cross-validation scores and hyperparamters from a grid or random search"""
+    for test in sorted(tuned_estimator.grid_scores_, key=itemgetter(1), reverse=reverse):
+        print "Validation score {:.2f} +/- {:.2f}, Hyperparams {}".format(100. * test.mean_validation_score,
+                                                                          100. * test.cv_validation_scores.std(),
+                                                                          test.parameters)
 
 def main():
     args = parse_args()
@@ -149,7 +167,7 @@ def main():
     splits = sklearn.cross_validation.LeaveOneLabelOut(train_data["file_number"])
 
     # just use the biggest one for now
-    X_train, Y_train = separate_output(train_data)
+    X_train, Y_train = separate_output(train_data, num_outputs=3)
 
     if args.extra_analysis:
         X_train.info()
@@ -171,37 +189,43 @@ def main():
     scores = mse_to_rms(sklearn.cross_validation.cross_val_score(model, X_train, Y_train, scoring="mean_squared_error", cv=splits))
     print "LinearRegression: {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
 
-    # model = sklearn.linear_model.PassiveAggressiveRegressor()
-    # scores = sklearn.cross_validation.cross_val_score(model, X, Y, scoring="mean_squared_error", cv=splits)
-    # print "PA: {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
+    if args.extra_analysis:
+        model = MultivariateRegressionWrapper(sklearn.linear_model.LinearRegression())
+        wrapper_scores = mse_to_rms(sklearn.cross_validation.cross_val_score(model, X_train, Y_train, scoring="mean_squared_error", cv=splits))
+        print "\tMultivariate wrapper change on LinearRegression: {}".format((wrapper_scores - scores).mean())
+
+    model = MultivariateRegressionWrapper(sklearn.ensemble.BaggingRegressor(sklearn.linear_model.LinearRegression(), max_samples=0.9, max_features=0.5))
+    cross_validate(X_train, Y_train, model, "Bagging(LinearRegression)", splits)
 
     model = sklearn.ensemble.RandomForestRegressor(20, min_samples_leaf=100, max_depth=3)
-    scores = mse_to_rms(sklearn.cross_validation.cross_val_score(model, X_train, Y_train, scoring="mean_squared_error", cv=splits))
-    print "RandomForestRegression: {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
+    cross_validate(X_train, Y_train, model, "RandomForestRegressor", splits)
 
-    # rf_hyperparams = {
-    #     "min_samples_leaf": scipy.stats.randint(10, 100),
-    #     "max_depth": scipy.stats.randint(3, 10),
-    #     "max_features": ["sqrt", "log2", 1.0, 0.3],
-    #     "n_estimators": scipy.stats.randint(20, 100)
-    # }
-    # wrapped_model = sklearn.grid_search.RandomizedSearchCV(model, rf_hyperparams, n_iter=10, n_jobs=3)
-    # scores = mse_to_rms(sklearn.cross_validation.cross_val_score(wrapped_model, X_train, Y_train, scoring="mean_squared_error", cv=splits))
-    # print "RandomForestRegression(tuned): {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
+    rf_hyperparams = {
+        "min_samples_leaf": scipy.stats.randint(10, 100),
+        "max_depth": scipy.stats.randint(3, 10),
+        "max_features": ["sqrt", "log2", 1.0, 0.3],
+        "n_estimators": scipy.stats.randint(20, 100)
+    }
+    wrapped_model = sklearn.grid_search.RandomizedSearchCV(model, rf_hyperparams, n_iter=10, n_jobs=3)
+    cross_validate(X_train, Y_train, wrapped_model, "RandomizedSearchCV(RandomForestRegression)", splits)
 
-    # BROKEN! GradientBoosting only handles univariate output
-    # model = VectorRegression(sklearn.ensemble.GradientBoostingRegressor())
-    # scores = mse_to_rms(sklearn.cross_validation.cross_val_score(model, X_train, Y_train, scoring="mean_squared_error", cv=splits))
-    # print "GradientBoostingRegressor: {:.3f} +/- {:.3f}".format(scores.mean(), scores.std())
+    model = MultivariateRegressionWrapper(sklearn.ensemble.GradientBoostingRegressor())
+    cross_validate(X_train, Y_train, model, "GradientBoostingRegressor", splits)
+
+    if args.prediction_file != "-":
+        baseline_model.fit(X_train, Y_train)
+
+        # retrain a model on the full data
+        # model = sklearn.linear_model.LinearRegression()
+        model.fit(X_train, Y_train)
+
+        predict_test_data(model, scaler, args, Y_train, baseline_model=baseline_model)
 
 
-    baseline_model.fit(X_train, Y_train)
-
-    # retrain a model on the full data
-    # model = sklearn.linear_model.LinearRegression()
-    model.fit(X_train, Y_train)
-
-    predict_test_data(model, scaler, args, Y_train, baseline_model=baseline_model)
+def cross_validate(X_train, Y_train, model, model_name, splits):
+    scores = mse_to_rms(
+        sklearn.cross_validation.cross_val_score(model, X_train, Y_train, scoring="mean_squared_error", cv=splits))
+    print "{}: {:.3f} +/- {:.3f}".format(model_name, scores.mean(), scores.std())
 
 
 def predict_test_data(model, scaler, args, Y_train, baseline_model=None):
@@ -226,10 +250,14 @@ def predict_test_data(model, scaler, args, Y_train, baseline_model=None):
     test_data[Y_test.columns].to_csv(args.prediction_file, index_label="ut_ms")
 
 
-def separate_output(dataframe):
+def separate_output(dataframe, num_outputs=None):
     dataframe.drop("file_number", axis=1, inplace=True)
 
     Y = dataframe[[col for col in dataframe.columns if col.startswith("NPWD")]]
+    if num_outputs:
+        scores = collections.Counter({col: Y[col].mean() + Y[col].std() for col in Y.columns})
+        Y = Y[[col for col, _ in scores.most_common(num_outputs)]]
+
     X = dataframe[[col for col in dataframe.columns if not col.startswith("NPWD")]]
     return X, Y
 
