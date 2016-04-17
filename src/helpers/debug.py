b5c76e326a9beb91d4b4adb694f10ea76ad6f1c4
Keith Trnka <keith.trnka@gmail.com>
2016-04-16 20:36:13 -0700

    Fixing names and starting to work on multivariate but it's failing

--- src/helpers/debug.py
+++ src/helpers/debug.py
@@ -12,13 +12,14 @@ from sklearn.metrics import mean_absolute_error
 import sklearn.ensemble
 
 
-def explain_prediction(y_true, y_pred, x, model, eps=1e-4, param_eps=None, max_eps=20.):
+def get_input_gradient(y_true, y_pred, x, model, eps=1e-4, param_eps=None, max_eps=20.):
+    """Approximate numeric gradient on this one example with respect to the inputs (assumes the model weights are fixed)"""
     assert isinstance(x, numpy.ndarray)
     if param_eps is None:
         param_eps = numpy.ones_like(x) * eps
 
-    # squared error
-    base_error = (y_true - y_pred) ** 2
+    # MSE, should be a single number
+    base_error = ((y_true - y_pred) ** 2).mean()
 
     # build a perturbation matrix of size the number of elements
     pos_shifted = stack_all_perturbations(x, 0, perturb_elements=param_eps)
@@ -26,8 +27,12 @@ def explain_prediction(y_true, y_pred, x, model, eps=1e-4, param_eps=None, max_e
 
     perturbed_predictions = model.predict(pos_shifted)
 
-    # elementwise squared error
-    perturbed_error = (y_true - perturbed_predictions) ** 2
+    # for univariate some models will output a vector rather than 2d array so we need to fix that
+    if len(perturbed_predictions.shape) == 1:
+        perturbed_predictions = perturbed_predictions.reshape(-1, 1)
+
+    # MSE for each row of ppredictions
+    perturbed_error = ((y_true - perturbed_predictions) ** 2).mean(axis=1)
 
     d_error = (perturbed_error - base_error).flatten()
     d_x = x * param_eps / 2.
@@ -41,13 +46,13 @@ def explain_prediction(y_true, y_pred, x, model, eps=1e-4, param_eps=None, max_e
         wider_eps = numpy.minimum(scale * param_eps, numpy.ones_like(scale) * max_eps)
 
         if not numpy.array_equal(param_eps, wider_eps):
-            return explain_prediction(y_true, y_pred, x, model, param_eps=wider_eps, max_eps=max_eps)
+            return get_input_gradient(y_true, y_pred, x, model, param_eps=wider_eps, max_eps=max_eps)
 
     return input_gradients
 
 
-def explain_blame(feature_blame):
-    for i, derivative in enumerate(feature_blame):
+def explain_input_gradient(input_gradient):
+    for i, derivative in enumerate(input_gradient):
         print "To get desired prediction, the model wants you to adjust feature {} by {}".format(i, -derivative)
 
 
@@ -68,10 +73,14 @@ def stack_all_perturbations(x, perturb_all, perturb_elements=None, dtype=numpy.f
 
 class ExplanationTests(unittest.TestCase):
     @staticmethod
-    def _make_data(n):
-        base = numpy.random.rand(n, 1).flatten()
-        X = numpy.asarray([numpy.cumsum(numpy.ones_like(base)), numpy.ones_like(base)]).transpose()
-        Y = (base + X[:, 0]).reshape(-1, 1)
+    def _make_data(n, num_outputs=1):
+        ones = numpy.ones((n, 1))
+        X = numpy.hstack([numpy.cumsum(ones).reshape(ones.shape), ones])
+
+        Y = numpy.tile(X[:, 0], (num_outputs, 1)).transpose()
+        Y += numpy.random.rand(*Y.shape)
+
+        print X.shape, Y.shape
 
         return X, Y
 
@@ -100,8 +109,26 @@ class ExplanationTests(unittest.TestCase):
 
         # now diagnose all examples - it should be weighting the first feature the most
         for i in xrange(Y_test.shape[0]):
-            input_gradient = explain_prediction(Y_test[i], predictions[i], X_test[i], model)
+            input_gradient = get_input_gradient(Y_test[i], predictions[i], X_test[i], model)
             self.assertLess(abs(input_gradient[1]), abs(input_gradient[0]))
+
+    def test_explain_multivariate(self):
+        X, Y = self._make_data(100, 2)
+        X_train, X_test, Y_train, Y_test = sklearn.cross_validation.train_test_split(X, Y, test_size=0.2)
+
+        # train a model, no intercept so that it's easier to diagnose
+        model = sklearn.linear_model.LinearRegression(fit_intercept=False).fit(X_train, Y_train)
+        predictions = model.predict(X_test)
+
+        # check that it has a semi-ok fit
+        self.assertLess(mean_absolute_error(Y_test, predictions), 10)
+
+        # now diagnose all examples - it should be weighting the first feature the most
+        for i in xrange(Y_test.shape[0]):
+            input_gradient = get_input_gradient(Y_test[i], predictions[i], X_test[i], model)
+
+            self.assertLess(abs(input_gradient[1]), abs(input_gradient[0]))
+
 
     def test_explain_random_forest(self):
         X, Y = self._make_data(100)
@@ -115,5 +142,5 @@ class ExplanationTests(unittest.TestCase):
 
         # now diagnose all examples - it should be weighting the first feature the most
         for i in xrange(Y_test.shape[0]):
-            input_gradient = explain_prediction(Y_test[i], predictions[i], X_test[i], model)
+            input_gradient = get_input_gradient(Y_test[i], predictions[i], X_test[i], model)
             self.assertLess(abs(input_gradient[1]), abs(input_gradient[0]))
