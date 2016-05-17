33cd31629f1f458bb00baccd8e43276ad1786274
Keith Trnka <keith.trnka@gmail.com>
2016-05-16 17:33:17 -0700

    Fixing CV graph

--- src/graph_cv.py
+++ src/graph_cv.py
@@ -6,6 +6,7 @@ import logging
 import sys
 import argparse
 
+import io
 import pandas
 import sklearn
 
@@ -23,22 +24,10 @@ def parse_args():
     parser.add_argument("--verify", default=False, action="store_true", help="Run verifications on the input data for outliers and such")
     parser.add_argument("--resample", default="1H", help="Time interval to resample the training data")
     parser.add_argument("--extra-analysis", default=False, action="store_true", help="Extra analysis on the data")
+    parser.add_argument("--clip", default=None, help="File with clip values")
     parser.add_argument("training_dir", help="Dir with the training CSV files or joined CSV file with the complete feature matrix")
     parser.add_argument("base_output", help="Base filename for output graphs. The model name and resample interval will be attached.")
     return parser.parse_args()
-
-
-def main():
-    args = parse_args()
-    logging.basicConfig(level=logging.INFO)
-
-    dataset = load_split_data(args)
-
-    cv_graph(dataset, sklearn.linear_model.ElasticNet(0.01), args.base_output)
-    cv_graph(dataset, make_nn(), args.base_output)
-
-if __name__ == "__main__":
-    sys.exit(main())
 
 
 def cv_graph(dataset, model, graph_filename):
@@ -65,3 +54,31 @@ def cv_graph(dataset, model, graph_filename):
         axes = sampled_df.plot(figsize=(16, 9), ylim=(0, 2))
         axes.set_title("Top predictions")
         axes.get_figure().savefig(helpers.general._with_extra(helpers.sk.with_model_name(graph_filename, model, snake_case=True), "resample{}".format(resample)), dpi=300)
+
+
+def main():
+    args = parse_args()
+    logging.basicConfig(level=logging.INFO)
+
+    dataset = load_split_data(args)
+
+    elastic_net = sklearn.linear_model.ElasticNet(0.01)
+
+    if args.clip:
+        mlp = make_nn(add_clipper=False)
+
+        # load the clipper
+        with io.open(args.clip, "rb") as json_in:
+            clipper = helpers.sk.OutputClippedTransform.load(json_in)
+
+        # wrap the model with the clipper
+        elastic_net = helpers.sk.OutputTransformation(elastic_net, clipper)
+        mlp = helpers.sk.OutputTransformation(mlp, clipper)
+    else:
+        mlp = make_nn(add_clipper=True)
+
+    cv_graph(dataset, elastic_net, args.base_output)
+    cv_graph(dataset, mlp, args.base_output)
+
+if __name__ == "__main__":
+    sys.exit(main())
